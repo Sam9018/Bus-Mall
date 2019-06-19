baab3fe2c0db639e848f98c04d1d1f564dd62b72
Sam Gnuschke <Gnuschke@live.com>
2019-06-18 21:31:52 -0700

    Got chart working in JS

--- app.js
+++ app.js
@@ -1,105 +1,177 @@
 'use strict';
 
+var maxClicksAllowed = 25;
+var clicksThisSession = 0;
 var previousSetOfImages = [];
-
 
 function ImageAnalytics(name, filepath) {
   this.name = name;
   this.filepath = filepath;
+  this.displayed = 0;
+  this.clicked = 0;
   ImageAnalytics.imageDatabase.push(this);
 }
-
 ImageAnalytics.imageDatabase = [];
 
-//CHANGE FILE PATH
-new ImageAnalytics('R2D2 Bag', 'images/bag.jpg', 0, 0);
-new ImageAnalytics('Banana Slicer', 'images/banana.jpg', 0, 0);
-new ImageAnalytics('Bathroom Screen', './images/bathroom.jpg', 0, 0);
-new ImageAnalytics('Yellow Boots', './images/boots.jpg', 0, 0);
-new ImageAnalytics('Breakfast Toaster', './images/breakfast.jpg', 0, 0);
-new ImageAnalytics('Delicious Meatball Gum, Yum!', './images/bubblegum.jpg', 0, 0);
-new ImageAnalytics('Gorgeous Red Chair', './images/chair.jpg', 0, 0);
-new ImageAnalytics('Cthulhu Eats a Guy!', './images/cthulhu.jpg', 0, 0);
-new ImageAnalytics('Duck Dog Muzzle', './images/dog-duck.jpg', 0, 0);
-new ImageAnalytics('YUMMY dragon meat', './images/dragon.jpg', 0, 0);
-new ImageAnalytics('Office Cutlery', './images/pen.jpg', 0, 0);
-new ImageAnalytics('Doggie Housework Helper', './images/pet-sweep.jpg', 0, 0);
-new ImageAnalytics('Pizza Slicer', './images/scissors.jpg', 0, 0);
-new ImageAnalytics('Sleep With the Fishes', './images/shark.jpg', 0, 0);
-new ImageAnalytics('Put Baby to WORK', './images/sweep.png', 0, 0);
-new ImageAnalytics('Smells Worse Inside', './images/tauntaun.jpg', 0, 0);
-new ImageAnalytics('You\'re a Monster if You Eat This', './images/unicorn.jpg', 0, 0);
-new ImageAnalytics('Slither', './images/usb.gif', 0, 0);
-new ImageAnalytics('Not Useless at ALL', './images/water-can.jpg', 0, 0);
-new ImageAnalytics('Stylish', './images/wine-glass.jpg', 0, 0);
-
-console.log(ImageAnalytics.imageDatabase);
-
-var imageContainer = document.getElementById(images);
-imageContainer.addEventListener('click', handleClick);
+new ImageAnalytics('R2D2 Bag', 'images/bag.jpg');
+new ImageAnalytics('Banana Slicer', 'images/banana.jpg');
+new ImageAnalytics('Bathroom Screen', './images/bathroom.jpg');
+new ImageAnalytics('Yellow Boots', './images/boots.jpg');
+new ImageAnalytics('Breakfast Toaster', './images/breakfast.jpg');
+new ImageAnalytics('Delicious Meatball Gum, Yum!', './images/bubblegum.jpg');
+new ImageAnalytics('Gorgeous Red Chair', './images/chair.jpg');
+new ImageAnalytics('Cthulhu Eats a Guy!', './images/cthulhu.jpg');
+new ImageAnalytics('Duck Dog Muzzle', './images/dog-duck.jpg');
+new ImageAnalytics('YUMMY dragon meat', './images/dragon.jpg');
+new ImageAnalytics('Office Cutlery', './images/pen.jpg');
+new ImageAnalytics('Doggie Housework Helper', './images/pet-sweep.jpg');
+new ImageAnalytics('Pizza Slicer', './images/scissors.jpg');
+new ImageAnalytics('Sleep With the Fishes', './images/shark.jpg');
+new ImageAnalytics('Put Baby to WORK', './images/sweep.png');
+new ImageAnalytics('Smells Worse Inside', './images/tauntaun.jpg');
+new ImageAnalytics('You\'re a Monster if You Eat This', './images/unicorn.jpg');
+new ImageAnalytics('Slither', './images/usb.gif');
+new ImageAnalytics('Not Useless at ALL', './images/water-can.jpg');
+new ImageAnalytics('Stylish', './images/wine-glass.jpg');
 
 
+function setupListeners() {
+  var imageContainer = document.getElementById('images');
+  imageContainer.addEventListener('click', handleClick);
+}
+
+function removeListeners() {
+  var imageContainer = document.getElementById('images');
+  imageContainer.removeEventListener('click', handleClick);
+}
 
 function getRandomNumber() {
-  return Math.floor(Math.random()*ImageAnalytics.imageDatabase.length);
+  return Math.floor(Math.random() * ImageAnalytics.imageDatabase.length);
 }
 
 function handleClick(event) {
-  console.log(event.target.id);
-  for(var i=0; i<ImageAnalytics.imageDatabase.length; i++) {
+  // console.log('i got here');
+  for (var i = 0; i < ImageAnalytics.imageDatabase.length; i++) {
     if (ImageAnalytics.imageDatabase[i].name === event.target.alt) {
+      // console.log(event.target.alt, 'event.target.alt');
       ImageAnalytics.imageDatabase[i].clicked++;
+      clicksThisSession++;
+      // console.log(clicksThisSession);
+      if (clicksThisSession === maxClicksAllowed) {
+        // console.log('insideInnerIf');
+        removeListeners();
+        createChart();
+      }
       break;
     }
   }
-  getRandomImage();
+  getRandomImages();
 }
 
+function getRandomImages() {
 
+  // The list of image ids from the DOM
+  var images = ['img1', 'img2', 'img3'];
 
+  // start with empty containers
+  var currentSetOfImages = [];
 
-
-function getRandomImage() {
-
-  var images = ('img1', 'img2', 'img3');
-  
+  // Repeat on each container
   for (var i = 0; i < images.length; i++) {
-    var image = document.getElementById(images[1]);
-    var randomNumber = getRandomNumber();
-    image.src = ImageAnalytics.imageDatabase(randomNumber).filepath;
-    image.alt = ImageAnalytics.imageDatabase(randomNumber).name;
-    randomArray.push(ImageAnalytics.imageDatabase[getRandomNumber()].filepath);
+
+    // Find it in the DOM
+    var image = document.getElementById(images[i]);
+
+    // Check if false
+    var ok = false;
+
+    // Run until unique image shown
+    while (ok === false) {
+
+      // Get a random number between 0 and the number of images
+      var randomNumber = getRandomNumber();
+
+      // If not from previous set and not on screen
+      if (!previousSetOfImages.includes(randomNumber) && !currentSetOfImages.includes(randomNumber)) {
+
+        // Update it's displayed count
+        ImageAnalytics.imageDatabase[randomNumber].displayed++;
+
+        // Render it
+        image.src = ImageAnalytics.imageDatabase[randomNumber].filepath;
+        image.alt = ImageAnalytics.imageDatabase[randomNumber].name;
+
+        // Add it to my list of seen images
+        currentSetOfImages.push(randomNumber);
+
+        // Bug out
+        ok = true;
+      }
+
+    }
   }
+
+  previousSetOfImages = currentSetOfImages;
+
 }
 
-getRandomImage();
-
-var left = document.getElementById('img1');
-var center = document.getElementById('img2');
-var right = document.getElementById('img3');
+setupListeners();
+getRandomImages();
 
 
 
 
-// var imagesArray = [
-//   './images/bag.jpg',
-//   './images/banana.jpg',
-//   './images/bathroom.jpg',
-//   './images/boots.jpg',
-//   './images/breakfast.jpg',
-//   './images/bubblegum.jpg',
-//   './images/chair.jpg',
-//   './images/cthulhu.jpg',
-//   './images/dog-duck.jpg',
-//   './images/dragon.jpg',
-//   './images/pen.jpg',
-//   './images/pet-sweep.jpg',
-//   './images/scissors.jpg',
-//   './images/shark.jpg',
-//   './images/sweep.png',
-//   './images/tauntaun.jpg',
-//   './images/unicorn.jpg',
-//   './images/usb.gif',
-//   './images/water-can.jpg',
-//   './images/wine-glass.jpg',
-// ];
+
+
+
+var ctx = document.getElementById('chart').getContext('2d');
+
+function createChart() {
+  var labels = [];
+  var displayedTimes = [];
+  var clickedTimes = [];
+  var colors = [];
+
+  for (var i = 0; i < ImageAnalytics.imageDatabase.length; i++) {
+    labels.push(ImageAnalytics.imageDatabase[i].name);
+    displayedTimes.push(ImageAnalytics.imageDatabase[i].displayed);
+    clickedTimes.push(ImageAnalytics.imageDatabase[i].clicked)
+    var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    colors.push(randomColor);
+  }
+
+
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Number of times displayed',
+          data: displayedTimes,
+          backgroundColor: '#ff0000',
+        },
+        {
+          label: 'Number of times clicked',
+          data: clickedTimes,
+          backgroundColor: colors,
+        }
+      ]
+    },
+    options: {
+      responsive: false,
+      maintainAspectRatio: true,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+
+}
+
